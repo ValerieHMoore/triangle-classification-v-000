ac6bee028cdb6c980af8e33080b5667ce319528c
Valerie Moore <periwinkle17@outlook.com>
2018-07-12 22:59:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    # if triangle.class != Triangle 
+    #   begin
+    #     raise TriangleError
+    #   rescue TriangleError => error
+    #       puts error.message
+    #   end
+    if (length1 == length2) && (length2 == length3)
+      :equilateral
+    elsif (length1 != length2) && (length2 == length3)
+      :isosceles
+    elsif (length2 != length3) && (length1 == length3)
+      :isosceles
+    elsif (length1 != length3) && (length1 == length2)
+      :isosceles
+    elsif (length1 != length2) && (length2 != length3) && (length1 != length3)
+      :scalene
+    end
+  end
+  
+    def valid?
+      if    ((length1 + length2) > length3) && (length1 > 0 && length2 > 0 && length3 > 0)
+      elsif ((length2 + length3) > length1) && (length1 > 0 && length2 > 0 && length3 > 0)
+      elsif ((length1 + length3) > length2) && (length1 > 0 && length2 > 0 && length3 > 0)
+      end
+    end
+  
+  # def true_triangle
+  #   real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  #   [a, b, c].each { |s| real_triangle << false if s <= 0 }
+  #   raise TriangleError if real_triangle.include?(false)
+  # end
+
+class TriangleError < StandardError
+    def self.message
+    "Not a valid triangle!"
+    end
+  end
+
 end
