3a20900005d3a4cb2df4a9008570de8511222b99
Valerie Moore <periwinkle17@outlook.com>
2018-07-12 23:37:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -32,9 +32,10 @@ class Triangle
   end
   
     def valid?
-      if    length1 + length2 > length3 && length1 > 0 && length2 > 0 && length3 > 0
-      elsif length2 + length3 > length1 && length1 > 0 && length2 > 0 && length3 > 0
-      elsif length1 + length3 > length2 && length1 > 0 && length2 > 0 && length3 > 0
+      if length1 > 0 && length2 > 0 && length3 > 0 &&
+        (length1 + length2 > length3) ||
+        (length2 + length3 > length1) ||
+        (length1 + length3 > length2)
       end
     end
   
