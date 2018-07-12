c4b655a2dc860341576d419a6d07373b9c8589d2
Valerie Moore <periwinkle17@outlook.com>
2018-07-12 23:29:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,12 +10,12 @@ class Triangle
   end
 
   def kind
-    if triangle.class != Triangle 
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+    # if triangle.class != Triangle 
+    #   begin
+    #     raise TriangleError
+    #   rescue TriangleError => error
+    #       puts error.message
+    #   end
     if (length1 == length2) && (length2 == length3)
       :equilateral
     elsif (length1 != length2) && (length2 == length3)
